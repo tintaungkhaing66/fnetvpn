0388ab91cd06e44f0e916af78b7cf081b167bc98
tintaungkhaing66 <114971359+tintaungkhaing66@users.noreply.github.com>
2022-10-04 10:37:15 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>FNET VPN | TH</title>
+  
+ <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <link rel="stylesheet" href="https://fnetvpn-tool.netlify.app/css/style.css">
+</head>
+
+    <body>
+        <center>
+<div class='sora-author-box'>
+<div class='sora-author-box-image'></div>
+<img src="https://fnetvpn-tool.netlify.app/img/fnetvpn1.webp" class="w3-round-xlarge" alt="Norway" style="width:100%">
+<div class='sora-author-box-text1'>
+<div class='sora-author-box-text-head'>
+</br>
+<span class='hello-txt'>NEW 🎉🎉</span>
+<h2 class='profile-title'><span></span>FNET VPN</h2>
+<h3 class='profile-position'>Thailand &#9989;</h3>
+</div>
+
+<div class='sora-author-box-text '>
+<div class='sora-author-box-text-details'>
+<ul class='profile-list'>
+<li class='clearfix'>
+<strong class='list-title'><span class='list-button'>Size</span></strong>
+<span class='cont'><i class='fa fa-file'></i>6.42 MB</span>
+</li>
+<li class='clearfix'>
+<strong class='list-title'><span class='list-button'>Version</span></strong>
+<span class='cont'><i class='fa fa-edit'></i>1.9</span>
+</li>
+<li class='clearfix'>
+<strong class='list-title'><span class='list-button'>Updated</span></strong>
+<span class='cont'><i class='fa fa-calendar'></i>March 25, 2022</span>
+</li>
+<li class='clearfix'>
+<strong class='list-title'><span class='list-button'>Requires</span></strong>
+<span class='cont'><i class='fa fa-android'></i>4.1 and up</span>
+</li>
+<li class='clearfix'>
+<strong class='list-title'><span class='list-button'>Developer</span></strong>
+<span class='cont'><i class='fa fa-code'></i><a href='https://www.facebook.com/iamHtetz' style='color: rgb( 157,160,167)'><font color='#9da0a7'>Tint Aung Khaing</font></a></span>
+</li>
+</ul>
+</div>
+</div></div>
+<div class='sora-cv-author-details wow slideInUp row'>
+<div style='clear: both;'></div>
+<div class='card-action' id='about-btn'>
+
+<div class='about-btn'>
+<a class='btn waves-effect' href='https://fnetvpn.github.io/project1/assets/app/FNETVPN_1.9.apk'>Direct Download</a>
+</div>
+</div>
+</div>
+</body>
+<style>
+</style>
