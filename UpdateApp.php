c2ca7686a5662292810c0f31a0036391f6ddbf8a
tintaungkhaing66 <114971359+tintaungkhaing66@users.noreply.github.com>
2022-11-09 18:33:05 +0700

    Update UpdateApp.php

--- UpdateApp.php
+++ UpdateApp.php
@@ -1,6 +1,6 @@
 {
   
-"message":"FNET VPN ဗားရှင်း အသစ်အားဒေါင်းယူပါ\nဒေါင်းယူရန် UPDATE ကို နှိပ်ပါ\nဒီလင့်ကို နှိပ်ပြီးလည်း ရယူနိုင်ပါတယ်👇👇\nhttps://m.apkpure.com/maxvpn-pro-safe-unlimited/sansoe.maxvpnpro.net\n \nThanks for your understand❤️\n",
+"message":"FNET VPN ဗားရှင်း အသစ်အားဒေါင်းယူပါ\nဒေါင်းယူရန် UPDATE ကို နှိပ်ပါ\n\nဒီလင့်ကို နှိပ်ပြီးလည်း ရယူနိုင်ပါတယ်👇👇\nhttps://m.apkpure.com/maxvpn-pro-safe-unlimited/sansoe.maxvpnpro.net\n \nThanks for your understand❤️\n",
    "title":"New app version available",
    "link":"play.google.com/store/apps/details?id=sansoe.maxvpnpro.net",
    "version": 2
