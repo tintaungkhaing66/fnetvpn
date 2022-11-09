ffb8e8fecb472df527fc6fddce73452af0828c50
tintaungkhaing66 <114971359+tintaungkhaing66@users.noreply.github.com>
2022-11-10 06:28:42 +0700

    Update UpdateApp.php

--- UpdateApp.php
+++ UpdateApp.php
@@ -3,5 +3,5 @@
 "message":"FNET VPN ဗားရှင်း အသစ်အားဒေါင်းယူပါ\nဒေါင်းယူရန် UPDATE ကို နှိပ်ပါ\n\nWait\nWait..\nWait...\nGet new app version to use this App preparation\n",
    "title":"New app version available",
    "link":"play.google.com/store/apps/details?id=sansoe.maxvpnpro.net",
-   "version": 2
+   "version": 3
 }
